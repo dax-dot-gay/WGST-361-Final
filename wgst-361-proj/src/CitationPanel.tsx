d178983f511647bfc5bdc376106aa4fbf2ccd484
Dax Harris <mvh3875@rit.edu>
2025-05-02 14:44:40 -0400

    second slide and more sources

--- wgst-361-proj/src/CitationPanel.tsx
+++ wgst-361-proj/src/CitationPanel.tsx
@@ -1,41 +1,63 @@
-import { Stack } from "@mantine/core";
+import { ScrollAreaAutosize, Stack } from "@mantine/core";
 import { Citation } from "./components/Citations";
 
 export function CitationPanel() {
     return (
-        <Stack gap="sm" p="sm" className="citation-list">
-            <Citation citeKey={1} title="Of Molecules and Sex" url="/assets/sources/of_molecules_and_sex.pdf">
-                Fausto-Sterling, Anne. “Of Molecules and Sex,” in Sex/Gender: Biology in a Social World. Abingdon, Oxon: Taylor
-                & Francis Group, 2012
-            </Citation>
-            <Citation
-                citeKey={2}
-                title="Constructions of Sex and Gender: Attending to Androgynes and ‘Tumtumim’ through Jewish Scriptural Use."
-                url="/assets/sources/jewish_sex_and_gender.pdf"
-            >
-                Schleicher, Marianne. “Constructions of Sex and Gender: Attending to Androgynes and ‘Tumtumim’ through Jewish
-                Scriptural Use.” Literature and Theology 25, No. 4, (December 2011), pp. 422-435.
-            </Citation>
-            <Citation citeKey={3} title="Sex Wars and (Trans) Gender Panics" url="/assets/sources/sex_wars_trans_panics.pdf">
-                Hines, Sally. “Sex wars and (trans) gender panics: Identity and body politics in contemporary UK feminism.” The
-                Sociological Review Monographs 68 no. 4 (2020): 699–717.
-            </Citation>
-            <Citation
-                citeKey={4}
-                title="How Sex Changed - From Sex to Gender (Excerpt)"
-                url="/assets/sources/from_sex_to_gender.pdf"
-            >
-                Meyerowitz, Joanne. How Sex Changed: A History of Transsexuality in the United States. 1;1st Harvard University
-                Press pbk.; ed. Cambridge, MA: Harvard University Press, 2004;2002;2009.
-            </Citation>
-            <Citation
-                citeKey={5}
-                title="Settler Homonationalism: Theorizing Settler Colonialism within Queer Modernities"
-                url="/assets/sources/settler_colonialism_queer_modernity.pdf"
-            >
-                Morgensen, Scott Lauria. "Settler Homonationalism: Theorizing Settler Colonialism within Queer Modernities." GLQ
-                16, no. 1-2 (2010): 105-131.
-            </Citation>
-        </Stack>
+        <ScrollAreaAutosize mah="calc(100vh - 183px)">
+            <Stack gap="sm" p="sm" className="citation-list">
+                <Citation citeKey={1} title="Of Molecules and Sex" url="/assets/sources/of_molecules_and_sex.pdf">
+                    Fausto-Sterling, Anne. “Of Molecules and Sex,” in Sex/Gender: Biology in a Social World. Abingdon, Oxon:
+                    Taylor & Francis Group, 2012
+                </Citation>
+                <Citation
+                    citeKey={2}
+                    title="Constructions of Sex and Gender: Attending to Androgynes and ‘Tumtumim’ through Jewish Scriptural Use."
+                    url="/assets/sources/jewish_sex_and_gender.pdf"
+                >
+                    Schleicher, Marianne. “Constructions of Sex and Gender: Attending to Androgynes and ‘Tumtumim’ through
+                    Jewish Scriptural Use.” Literature and Theology 25, No. 4, (December 2011), pp. 422-435.
+                </Citation>
+                <Citation
+                    citeKey={3}
+                    title="Sex Wars and (Trans) Gender Panics"
+                    url="/assets/sources/sex_wars_trans_panics.pdf"
+                >
+                    Hines, Sally. “Sex wars and (trans) gender panics: Identity and body politics in contemporary UK feminism.”
+                    The Sociological Review Monographs 68 no. 4 (2020): 699–717.
+                </Citation>
+                <Citation
+                    citeKey={4}
+                    title="How Sex Changed - From Sex to Gender (Excerpt)"
+                    url="/assets/sources/from_sex_to_gender.pdf"
+                >
+                    Meyerowitz, Joanne. How Sex Changed: A History of Transsexuality in the United States. 1;1st Harvard
+                    University Press pbk.; ed. Cambridge, MA: Harvard University Press, 2004;2002;2009.
+                </Citation>
+                <Citation
+                    citeKey={5}
+                    title="Settler Homonationalism: Theorizing Settler Colonialism within Queer Modernities"
+                    url="/assets/sources/settler_colonialism_queer_modernity.pdf"
+                >
+                    Morgensen, Scott Lauria. "Settler Homonationalism: Theorizing Settler Colonialism within Queer Modernities."
+                    GLQ 16, no. 1-2 (2010): 105-131.
+                </Citation>
+                <Citation
+                    citeKey={6}
+                    title="Radical Sovereignty, Rhetorical Borders, and the Everyday Decolonial Praxis of Indigenous Peoplehood and Two-Spirit Reclamation"
+                    url="/assets/sources/radical_sovereignty.pdf"
+                >
+                    Ellasante, Ian Khara. "Radical Sovereignty, Rhetorical Borders, and the Everyday Decolonial Praxis of
+                    Indigenous Peoplehood and Two-Spirit Reclamation." Ethnic and Racial Studies 44 no. 9 (2021): 1507-1526.
+                </Citation>
+                <Citation
+                    citeKey={7}
+                    title="Stolen From Our Bodies: First Nations Two-Spirits/Queers and the Journey to a Sovereign Erotic"
+                    url="/assets/sources/stolen_bodies.pdf"
+                >
+                    Driskill, Quo-li. “Stolen From Our Bodies: First Nations Two-Spirits/Queers and the Journey to a Sovereign
+                    Erotic.” Studies in American Indian Literatures 16, no. 2 (2004): 50–64.
+                </Citation>
+            </Stack>
+        </ScrollAreaAutosize>
     );
 }
--- wgst-361-proj/src/slides/Slide2.tsx
+++ wgst-361-proj/src/slides/Slide2.tsx
@@ -1,10 +1,106 @@
-import { Title } from "@mantine/core";
+import { Accordion, AccordionControl, AccordionItem, AccordionPanel, Alert, Group, Stack, Text } from "@mantine/core";
 import { Slide } from "./Slide";
+import { TbExclamationCircleFilled, TbHeartFilled, TbLanguage } from "react-icons/tb";
+import { Cite, CitationSummary } from "../components/Citations";
 
 export function Slide2() {
     return (
         <Slide name="slide-2" next="slide-3" title="Gender/Sexual Normativity as a Facet of Colonialism">
-            <Title order={3}>Assimilation of Identity</Title>
+            <Text>
+                The assimilation of indigenous peoples and cultures as a result of colonization is a well-understood practice,
+                with effects propagating through almost every facet of Western culture. In a very real sense, the "first world"
+                and its ideals are colonialism incarnate -- Western society positions itself as the "end of history", an imposed
+                assumption of perfection that many use to justify continued assimilation efforts. What is less frequently
+                acknowledged, however, is the effects that a colonizer will have on the values & ideals of those who it
+                assimilates, often through more indirect means than they might assimilate land or other physical resources.
+                While this can take a number of forms, several relevant to gender and sexuality are described below.
+            </Text>
+            <Accordion variant="separated" defaultValue={["f1"]} multiple>
+                <AccordionItem value="f1">
+                    <AccordionControl icon={<TbLanguage size={24} />}>Assimilation of Language</AccordionControl>
+                    <AccordionPanel>
+                        <Stack gap="sm">
+                            <Text>
+                                Across the political & social spectrum in Western culture, it is a common practice to apply
+                                Western linguistic frameworks (currently that of American English, in much of the world) to the
+                                process of describing the experiences of those suffering from colonization. This practice has
+                                been described as "rhetorical imperialism," a process by which colonizing powers "[set] the
+                                terms of debate" <Cite cite={6} name="Ellasante" page={1509} />. A particularly relevant example
+                                of this practice in the queer space is the categorization of Two-Spirit individuals (among
+                                others, such as <em>Takatāpui</em>, <em>Fa’afafine</em>, and more) under the umbrella of
+                                "transgender," despite their distinct differences from that predominantly Western concept. The
+                                argument can be made that this may help the Western audience to understand these concepts more
+                                easily, but in reality it more often leads to wide-ranging redefinition of indigenous language
+                                that fails to even remotely describe the appropriate context.
+                            </Text>
+                            <CitationSummary
+                                cite={6}
+                                title="Radical Sovereignty, Rhetorical Borders, and the Everyday Decolonial Praxis of Indigenous Peoplehood and Two-Spirit Reclamation"
+                                author="Ian Khara Ellasante"
+                            >
+                                In <em>Radical Sovereignty</em>, Ellasante details in great depth the concepts of "rhetorical
+                                sovereignty" and "rhetorical imperialism" -- concepts crucial to understanding colonization as
+                                it applies to the linguistic basis of cultures undergoing colonization. They use the example of
+                                Two-Spirit individuals as a centerpoint in this discussion, illuminating its status as a term
+                                meant to "challenge the settler-defined" language that has intertwined itself into the
+                                understanding of LGBTQ experiences <Cite cite={6} name="Ellasante" page={1519} />. In short,
+                                they argue that rhetorical imperialism is a key tool in eliminating indigenous agency, which can
+                                be strongly connected to other more obvious efforts by settler states to deny the nationhood of
+                                those they've colonized.
+                            </CitationSummary>
+                        </Stack>
+                    </AccordionPanel>
+                </AccordionItem>
+                <AccordionItem value="f2">
+                    <AccordionControl icon={<TbHeartFilled size={24} />}>Assimilation of Sexuality</AccordionControl>
+                    <AccordionPanel>
+                        <Stack gap="sm">
+                            <Text>
+                                In addition to rhetorical assimilation, colonizing forces generally seek to impose their own
+                                values on those they assimilate. In most cases, these ideals are fundamentally incompatible with
+                                said cultures, resulting in the cultural ideals of indigenous groups being forcibly altered,
+                                repurposed, or even erased entirely. This is especially important when it comes to sexual
+                                values, where the overwhelmingly puritanical & patriarchal values of modern Western culture come
+                                into direct conflict with the values of various indigenous societies -- with devastating
+                                results. A chief example of this is the increased frequency of sexual violence and sexism in
+                                American Indian reservations in the US, grim reflections of the extreme levels of sexism and
+                                abuse endemic to American culture <Cite cite={7} name="Driskill" page={53} />.
+                            </Text>
+                            <Group gap="sm" wrap="nowrap" align="start">
+                                <CitationSummary
+                                    cite={7}
+                                    title="Stolen From Our Bodies: First Nations Two-Spirits/Queers and the Journey to a Sovereign Erotic"
+                                    author="Qwo-li Driskill"
+                                    style={{ width: "calc(50% - 4px)" }}
+                                >
+                                    In this piece, Driskill condemns the imposition of colonial sexual values on the Cherokee,
+                                    and by extension all others that have been subject to the specter of Western imperialism.
+                                    They focus especially on the concept of “sovereign eroticism,” highlighting the damaging
+                                    influence that this “colonized sexuality” has had on the Cherokee culture and system of
+                                    values <Cite cite={7} name="Driskill" page={54} />. The piece as a whole quite effectively
+                                    conveys the centuries of colonial trauma that indigenous communities have suffered from the
+                                    colonization of their most basic systems of values, which can be easily extrapolated to the
+                                    great breadth of other atrocities committed against indigenous people – in America and
+                                    internationally.
+                                </CitationSummary>
+                                <Alert
+                                    icon={<TbExclamationCircleFilled size={20} />}
+                                    style={{ width: "calc(50% - 4px)" }}
+                                    title="Qwo-Li Driskill: Controversies"
+                                    color="yellow"
+                                >
+                                    Based on significant controversy in November of 2023, Qwo-Li Driskill is suspected to be a
+                                    "pretendian," or an individual who claims indigenous ancestry for personal or monetary
+                                    purposes. This accusation has been levied by the Tribal Alliance Against Frauds, which has
+                                    sufficient credibility to cast doubt on the cultural credentials that this author has to
+                                    discuss the matters in play. Despite this, however, several points made by Driskill's paper
+                                    are salient when validated against other sources, and so remain useful to this section.
+                                </Alert>
+                            </Group>
+                        </Stack>
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
         </Slide>
     );
 }
